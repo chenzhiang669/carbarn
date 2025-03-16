25c3ab82db538e989f8c514efa7549f4d8eee860
unknown <146151592@qq.com>
2025-03-16 21:28:19 +0800

    [优化]: 给用户增加默认头像

--- src/main/java/com/carbarn/inter/pojo/user/dto/SignupUserDTO.java
+++ src/main/java/com/carbarn/inter/pojo/user/dto/SignupUserDTO.java
@@ -7,6 +7,6 @@ public class SignupUserDTO {
     private String phone_num; //电话号码
     private String veri_code; //验证码
     private long user_count = 3423;
-    private String avatar; //头像
+    private String avatar = "http://image.chechuhai.top/carbarn/files/default_avatar/uVxyH4HqcP1740213823406.webp"; //头像
     private String nickname; //昵称
 }
